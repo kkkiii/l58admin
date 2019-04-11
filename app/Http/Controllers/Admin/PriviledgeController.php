89ba9cdc7bb11a81ec3ffddebf98d87fb6adbc78
liuhongshuo <2624886248@qq.com>
2019-04-11 18:49:51 +0800

    a

--- app/Http/Controllers/Admin/PriviledgeController.php
+++ app/Http/Controllers/Admin/PriviledgeController.php
@@ -18,17 +18,17 @@ class PriviledgeController extends Controller
     public function list(){
 
 
-        $url = URL::current() ;
-     $res =   Helpers::str_retrive_left($url , '/') ;
-print_r($res) ; die() ;
+//        $url = URL::current() ;
+//     $res =   Helpers::str_retrive_left($url , '/') ;
+//print_r($res) ; die() ;
 
 
-        var_dump($url) ;
-        $pos = strrpos ($url,'/') ;
-        var_dump($pos) ;
-      print_r(  substr($url , $pos) );
-
-        die() ;
+//        var_dump($url) ;
+//        $pos = strrpos ($url,'/') ;
+//        var_dump($pos) ;
+//      print_r(  substr($url , $pos) );
+//
+//        die() ;
 
         return view('admin.priviledge.list') ;
     }
