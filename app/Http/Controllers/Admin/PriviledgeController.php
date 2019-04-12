4bca699ca054dbaa04a5071759f74eb2f7ad35d5
liuhongshuo <2624886248@qq.com>
2019-04-12 18:28:27 +0800

    u

--- app/Http/Controllers/Admin/PriviledgeController.php
+++ app/Http/Controllers/Admin/PriviledgeController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
+use App\Model\AdminRole;
 use App\My\Helpers ;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\URL ;
+use Illuminate\Support\Facades\DB ;
 class PriviledgeController extends Controller
 {
     public function __construct()
@@ -32,4 +34,45 @@ class PriviledgeController extends Controller
 
         return view('admin.priviledge.list') ;
     }
+    public function roles(){
+
+        $roles = DB::table('admin_roles')->paginate(10);
+        return view('admin.priviledge.roles', ['roles' => $roles]);
+
+    }
+
+    public function edit_role($id){
+
+        $role = DB::table('admin_roles')->where('id', $id)->first();
+        return view('admin.priviledge.edit_role', ['role' => $role]);
+
+    }
+    public function edit_role_post(Request $request){
+
+//        $roles = DB::table('admin_roles')->paginate(10);
+//        return view('admin.priviledge.roles', ['roles' => $roles]);
+        $id =$request->input('id');
+        $name =$request->input('name');
+        var_dump($name) ;
+        $remark =$request->input('remark');
+        var_dump($remark) ;
+
+
+        $admin_role = AdminRole::find($id);
+        $admin_role->name = $name ;
+        $admin_role->remark = $remark ;
+        $admin_role->save();
+//        $post->title = '测试文章标题';
+//        $post->content = '测试文章内容';
+//        $post->user_id = 1;
+//        $post->save();
+        return redirect(route('priviledge.roles'));
+
+    }
+    public function del(Request $request,$id){
+      $admin_role =  AdminRole::find($id);
+        $admin_role->delete();
+        return redirect(route('priviledge.roles'));
+    }
+
 }
