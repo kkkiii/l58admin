ef7a808c9addc6c762c899ac711c1ae4e280e7d3
liuhongshuo <2624886248@qq.com>
2019-04-12 18:29:51 +0800

    a

--- config/auth.php
+++ config/auth.php
@@ -68,7 +68,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => \App\Model\YQUser::class,
+            'model' => \App\Model\Admin::class,
         ],
 
         // 'users' => [
